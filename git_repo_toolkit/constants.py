e290aa9b1d381fc58d201390d5fb90831bfe45fd
Muhammad Farhan Khan <mfarhan133@gmail.com>
2024-08-26 16:37:47 +0500

    chore: Update file utils function

--- git_repo_toolkit/constants.py
+++ git_repo_toolkit/constants.py
@@ -1,2 +1,5 @@
-SEARCH_OUTPUT_FILE_NAME = '../../output/search-result.txt'
-SEARCH_OUTPUT_CSV_FILE_NAME = '../../output/search-result.csv'
+import os
+
+OUTPUT_DIR_PATH = os.getenv('OUTPUT_DIR_PATH')
+SEARCH_OUTPUT_FILE_NAME = f"{OUTPUT_DIR_PATH}/search-result.txt"
+SEARCH_OUTPUT_CSV_FILE_NAME = f"{OUTPUT_DIR_PATH}/search-result.csv"
--- git_repo_toolkit/utils/file_utils.py
+++ git_repo_toolkit/utils/file_utils.py
@@ -1,3 +1,9 @@
+import os
+
+
 def save_in_file(file_name, content):
+    directory = os.path.dirname(file_name)
+    if not os.path.exists(directory) and directory != '':
+        os.makedirs(directory)
     with open(file_name, "w") as file:
         file.write(content)
